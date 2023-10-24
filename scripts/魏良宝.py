d0e55c48255d3511adecb4d2feab3fa61c1b6aa9
weiliangbao <148847680+weiliangbao@users.noreply.github.com>
2023-10-24 15:00:49 +0800

    魏良宝第一次作业脚本

---
+++ scripts/魏良宝.py
@@ -0,0 +1,18 @@
+# 生成模拟的建成区面积的Python脚本
+
+import pandas as pd
+import numpy as np
+# 整形变量
+year0 = 1990
+year1 =2023
+years = list(range(year0, year1))  #years 序列变量
+years = np.array(years) # np.array)
+builtupArea = years * 0.308 + np.random.randint(-100, 100) * 0.023
+# dictionary 映射/字典类型
+
+data = {
+"year": years,
+"builtupArea": builtupArea
+}
+pdf = pd.DataFrame(data = data)
+pdf.to_csv('builtupArea-魏良宝.csv', index=False)
