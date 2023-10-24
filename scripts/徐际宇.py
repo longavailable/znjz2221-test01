be9a962d22628342f2d0367f85093afe528c8ef5
88888888qaz <148839127+88888888qaz@users.noreply.github.com>
2023-10-24 16:14:44 +0800

    徐际宇第一次作业脚本

---
+++ scripts/徐际宇.py
@@ -0,0 +1,23 @@
+import numpy as np	#导入numpy模块，并创建别名np
+import pandas as pd
+
+# 整型变量
+year0 = 1990
+year1 = 2023
+
+
+years = list(range(year0, year1)) # years 序列变量
+
+years = np.array(years)	# np.array()
+
+population = years * 1000 + np.random.randint(-100, 100) * 100
+
+# dictionary 映射/字典类型
+data = {
+	'year': years,
+	'population': population
+}
+
+pdf = pd.DataFrame(data = data)
+
+pdf.to_csv('徐际宇-人口.csv', index=False)
