e5e27c371ba8ea6cf60a8f2e7c6ff9a812beacdd
GUANRUI7777 <149396223+GUANRUI7777@users.noreply.github.com>
2023-10-30 22:02:59 +0800

    关蕊的第一次作业

---
+++ data/关蕊-脚本.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pal
+＃整型变量
+yearo = 2002
+year/ =2023
+years = list(range(year0, year/))#
+years 序列变量
+years =np.array(years) # np.array)
+population = years * 1000 +
+nprandom.randint(-100, 100）* 0.025
+#dictionary映射/字典类型
+data. = {
+'year" years,
+'population': population
+}
+pdf = pd.DataFrame(data = data)
+pdt.to.csuCgr关蕊一人口.csU，
+index=False)
