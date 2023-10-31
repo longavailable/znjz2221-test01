62cbc1b78f7211c5e0e71ba4e4f89be17c6f89cc
zhengsijia888 <148838714+zhengsijia888@users.noreply.github.com>
2023-10-31 21:17:52 +0800

    郑思佳第一次脚本作业

---
+++ scripts/generate-builtupArea-郑思佳.py
@@ -0,0 +1,22 @@
+from turtle import pd
+
+import np as np
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+# 整型变量
+year0 = 2004
+year1 = 2023
+years = list(range(year0, year1))  # years 序列变量
+years = np.array(years)  # np.array()
+population = years * 1000 + np.random.randint(-100, 100) * 100
+# dictionary 映射/字典类型
+data = {
+    'year': years,
+    'population': population
+}
+pdf = pd.DataFrame(data=data)
+pdf.to_csv('郑思佳-人口。csv', index=False)
