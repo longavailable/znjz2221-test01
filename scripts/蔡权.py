b1be4658d339386645245b671134253eeb5518f4
CaiQvan <148838586+CaiQvan@users.noreply.github.com>
2023-11-07 13:54:21 +0800

    Add files via upload

--- scripts/蔡权.py
+++ scripts/蔡权.py
@@ -5,7 +5,7 @@ year0 = 1990
 year1 = 2023
 years = list(range(year0, year1))  #years 序列变量
 years = np.array(years) # np.array)
-builtupArea = years * 0.378 + np.random.randint(-100, 100) * 0.023
+builtupArea = years * 0.319 + np.random.randint(-100, 100) * 0.045
 # dictionary 映射/字典类型
 data = {
 "year": years,
