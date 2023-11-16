9840364f84632e53215838976acb7d92c710acc1
ToAn0602 <148861531+ToAn0602@users.noreply.github.com>
2023-11-16 16:06:59 +0800

    张德培的第一次作业脚本

--- scripts/张德培.py
+++ scripts/张德培.py
@@ -10,14 +10,14 @@ years = list(range(year0, year1)) # years 序列变量
 
 years = np.array(years)	# np.array()
 
-population = years * 1000 + np.random.randint(-100, 100) * 100
+builtupArea = years * 0.308 + np.random.randint(-100, 100) * 0.023
 
 # dictionary 映射/字典类型
 data = {
 	'year': years,
-	'population': population
+	'builtupArea': builtupArea
 }
 
 pdf = pd.DataFrame(data = data)
 
-pdf.to_csv('张德培-人口.csv', index=False)
+pdf.to_csv('builtupArea-张德培.csv', index=False)
